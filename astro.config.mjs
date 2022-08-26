0bf5b2769c1d52cd1eab0b8c82696f78c9606462
Maxime Koitsalu <maxime.koitsalu@gmail.com>
2022-08-26 10:55:19 +0000

    Refactor alpinejs cdn to astrojs/alpinejs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import alpine from '@astrojs/alpinejs';
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 
@@ -12,6 +13,7 @@ export default defineConfig({
   site: SITE.domain,
 
   integrations: [
+    alpine(),
     tailwind({
       config: {
         applyBaseStyles: false,
